a46d20387edbdceea0527729a72501f015643646
jgliang74 <jgliang74@gmail.com>
2018-07-13 13:59:37 -0700

    Changes to be committed:(car runs in simulator with given light's states) 	modified:   tl_detector/light_classification/tl_classifier.py 	modified:   tl_detector/tl_detector.py 	modified:   twist_controller/dbw_node.py 	modified:   twist_controller/twist_controller.py 	modified:   twist_controller/yaw_controller.py 	modified:   waypoint_follower/src/pure_pursuit_core.cpp 	modified:   waypoint_loader/waypoint_loader.py 	modified:   waypoint_updater/waypoint_updater.py

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,10 +1,32 @@
 from styx_msgs.msg import TrafficLight
+import numpy as np
+import os
+import tensorflow as tf
+import time
 
 class TLClassifier(object):
     def __init__(self):
-        #TODO load classifier
-        pass
+        self.loaded_model = False
+        model = 'Models/model_for_sim/frozen_inference_graph.pb'
+        
+        model_path = os.path.join(os.path.dirname(__file__), model)
+        self.detection_graph = tf.Graph()
 
+        # load model
+        with self.detection_graph.as_default():
+             od_graph_def = tf.GraphDef()
+             with tf.gfile.GFile(model_path, 'rb') as fid:
+                  serialized_graph = fid.read()
+                  od_graph_def.ParseFromString(serialized_graph)
+
+             self.session = tf.Session(graph=self.detection_graph)
+
+             # Define input and output Tensors for detection_graph
+             self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+             self.detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+             self.detectio_classes = self.detection_graph.get_tensor_by_name('num_detections:0')
+        self.loaded_model = True
+      
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
 
@@ -15,5 +37,48 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
+        image_width = image.shape[1]
+        cropped_image = image[0:int(round(image.shape[0]*0.7)),int(round(image.shape[1]*0.3)):image_width,:]
+
+        image_np_expanded = np.expand_dims(cropped_image, axis = 0)
+
+        # Traffic light detection.
+        (boxes, scores, classes, num) = self.session.run([self.detection_boxes, 
+                                                          self.detection_scores, 
+                                                          self.detection_classes,
+                                                          self.num_detections], 
+                                                          feed_dict={self.image_tensor: image_np_expanded})
+
+        scores = np.squeeze(scores)
+        classes = np.squeeze(classes).astype(np.int32)
+
+        score_threshold = 0.8
+        detection_with_goodscore = np.argwhere(scores > score_threshold)
+
+        if detection_with_goodscore.size > 0:
+           class_number = classes[detection_with_goodscore[0]]
+           if class_number == 2:
+              print('Detected Red light')
+              return TrafficLight.RED
+           elif class_number == 1:
+              print('Detected Green light')
+              return TrafficLight.GREEN
+           elif class_number == 3:
+              print('Detected Yellow light')
+              return TrafficLight.YELLOW
+
+        return TrafficLight.UNKNOWN
+
+
+
+
+
+
+
+ 
+
+
+
+
         #TODO implement light color prediction
         return TrafficLight.UNKNOWN
--- ros/src/twist_controller/yaw_controller.py
+++ ros/src/twist_controller/yaw_controller.py
@@ -1,4 +1,5 @@
 from math import atan
+import rospy
 
 class YawController(object):
     def __init__(self, wheel_base, steer_ratio, min_speed, max_lat_accel, max_steer_angle):
@@ -15,11 +16,15 @@ class YawController(object):
         angle = atan(self.wheel_base / radius) * self.steer_ratio
         return max(self.min_angle, min(self.max_angle, angle))
 
-    def get_steering(self, linear_velocity, angular_velocity, current_velocity):
+    def get_steering(self, linear_velocity, angular_velocity, current_velocity, currrent_angular_velocity):
         angular_velocity = current_velocity * angular_velocity / linear_velocity if abs(linear_velocity) > 0. else 0.
 
         if abs(current_velocity) > 0.1:
             max_yaw_rate = abs(self.max_lat_accel / current_velocity);
             angular_velocity = max(-max_yaw_rate, min(max_yaw_rate, angular_velocity))
 
-        return self.get_angle(max(current_velocity, self.min_speed) / angular_velocity) if abs(angular_velocity) > 0. else 0.0;
+        steering_angle = self.get_angle(max(current_velocity, self.min_speed) / angular_velocity) if abs(angular_velocity) > 0. else 0.
+        
+        steering_angle = steering_angle + 0.*(angular_velocity - currrent_angular_velocity)
+
+        return steering_angle
